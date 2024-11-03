88bcba4a32fab3e8a6664da48866fb60d5b5ff8e
Teekay <tkmaile96@gmail.com>
2024-11-03 16:49:55 +0200

    render html structures of the book preview

--- previewWC.js
+++ previewWC.js
@@ -1,1 +1,26 @@
-constr
+class BookPreview extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open'});
+    }
+
+    connectedCallback() {
+        this.render()
+    }
+
+    // observed attributes
+    static get observedAttributes() {
+        return [ 'title', 'author', 'image'];
+    }
+    
+    attributeChangedCallback() {
+        this.render()
+    }
+
+    /**
+     * render the html structures of the book preview.
+     */
+    render() {
+        
+    }
+}
